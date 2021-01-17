efa7fcf3d226b4711ed219c3da518c765b830e26
RomanDubina <ingylets@gmail.com>
2021-01-17 04:55:29 +0200

    desctop complete

--- app/js/main.js
+++ app/js/main.js
@@ -5,38 +5,57 @@ $(function () {
   });
   
   $(window).scroll(function () {
-    if ($(this).scrollTop() > 3000) {
+    if ($(this).scrollTop() > 2700) {
       // создаем эффекты и анимацию
       $(".bottle__line").css("display", "block");
       $(".bottle__line path").addClass("line-animation");
+      
     countCharacter();
+    dotAnimate();
       
     }
   });
 
+function dotAnimate(){
+    let dot1 = $('#dot-1');
+    let dot2 = $('#dot-2');
+    let dot3 = $('#dot-3');
+    dot1.css({
+        'animation' : `dotsAnimate .4s ease-in 1s`,
+        'animation-fill-mode': 'forwards'
+    })
+    dot2.css({
+        'animation' : `dotsAnimate .4s ease-in 2.7s`,
+        'animation-fill-mode': 'forwards'
+    })
+    dot3.css({
+        'animation' : `dotsAnimate .4s ease-in 4.1s`,
+        'animation-fill-mode': 'forwards'
+    })
+}
 
   function countCharacter(){
     let text1 = $('#line-text-1');
     let text2 = $('#line-text-2');
     let text3 = $('#line-text-3');
     let numCharT1 = text1.text().length;
-    let numCharT2 = text2.text().length
-    let numCharT3 = text3.text().length
+    let numCharT2 = text2.text().length;
+    let numCharT3 = text3.text().length;
 
     
     text1.css({
         'width' : `${numCharT1}ch`,
-        'animation' : `print-text .7s steps(${numCharT1}) 1.8s`,
+        'animation' : `print-text .7s steps(${numCharT1}) 1s`,
          'animation-fill-mode': 'forwards'
     })
     text2.css({
         'width' : `${numCharT2}ch`,
-        'animation' : `print-text .7s steps(${numCharT2}) 2.7s`,
+        'animation' : `print-text .7s steps(${numCharT2}) 2.4s`,
          'animation-fill-mode': 'forwards'
     })
     text3.css({
         'width' : `${numCharT3}ch`,
-        'animation' : `print-text .7s steps(${numCharT3}) 3.7s`,
+        'animation' : `print-text .7s steps(${numCharT3}) 3.8s`,
          'animation-fill-mode': 'forwards'
     })
 }
@@ -50,5 +69,13 @@ wow = new WOW(
   }
   )
 wow.init();
+
+$('.photos-slider__slick').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: true,
+    variableWidth: true,
+    arrows: true
+});
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -42,6 +42,7 @@ function scripts() {
         // сюда можно скачивать и подключать доп библиотеки JS & jQuery с NPM
         'node_modules/typeit/dist/typeit.min.js',
         'node_modules/wow.js/dist/wow.js',
+        'node_modules/slick-carousel/slick/slick.js',
         'app/js/main.js'
     ])
     .pipe(concat('main.min.js'))
@@ -55,6 +56,8 @@ function styles() {
     return src([
         'node_modules/normalize-css/normalize.css',
         'node_modules/animate.css/animate.css',
+        'node_modules/slick-carousel/slick/slick-theme.css',
+        'node_modules/slick-carousel/slick/slick.css',
         'app/scss/style.scss'
     ])
         .pipe(scss({outputStyle: 'compressed'}))// expanded
